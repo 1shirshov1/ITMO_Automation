31019ad1e289a2995a623f4ea22fcb9972431102
1shirshov1 <shirshovspb@ya.ru>
2023-04-21 21:31:18 +0300

    first

---
+++ python_trening/task_8_if_elif.py
@@ -0,0 +1,13 @@
+num_float = -1
+
+
+# также попробуйте варианты
+# num_float = 0
+# num_float = -4.5
+
+if num_float > 0:
+    print('положительное число')
+elif num_float == 0:
+    print('ноль')
+else:
+    print('число отрицательное')
